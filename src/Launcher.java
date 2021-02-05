05f2f8cd99e01ca56e434d3fcf9ce143ab57bd1e
renanboni <renan.boni@usp.br>
2021-02-05 20:08:06 +0100

    Fixings and added OpenGL support

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,6 +3,7 @@ import game.GameLoop;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "True");
         new Thread(new GameLoop(new Game(800, 600))).start();
     }
 }
